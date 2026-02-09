6e2b79b455b5d370abd52ced3c715ed83903294c
Shiva Rajan <shiva.rajan1560@gmail.com>
2026-02-09 15:09:01 +0530

    Adding Neetcode and update semantic tests for multiple documents

--- semantic_test.py
+++ semantic_test.py
@@ -14,6 +14,19 @@ def test_kubernetes_query():
 
     print("Kubernetes query test passed !!")
 
+def test_neetcode_query():
+    response = requests.post("http://localhost:8000/query?q=What is the Neetcode?")
+
+    if response.status_code != 200:
+        raise Exception(f"Server returned: {response.status_code} {response.text}")
+    
+    answer = response.json()["answer"]
+
+    #check for key concepts in the answer
+    assert "maximus" in answer.lower(), "Missing 'maximus' keyword"
+
+    print("Neetcode query test passed !!")
+
 if __name__ == "__main__":
     test_kubernetes_query()
     print("All semantic tests passed !!")
